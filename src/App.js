44c2f2882a8f5db8d0c98323cf8a2d2c97af4a84
lolmos <lol@canicacuadrada.com>
2020-04-27 14:11:41 +0200

    adding header component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component'
 
 const HatsPage = () => (
   <div>
@@ -14,9 +16,11 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.component';
+
+
+const SignInAndSignUpPage = ()=> (
+
+    <div className='sign-in-and-sign-up'>SIGN IN</div>
+
+);
+
+export default SignInAndSignUpPage;
