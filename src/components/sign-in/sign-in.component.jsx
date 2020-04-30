3275deca59b2430b74877d7d1a1eec09b46c519b
lolmos <lol@canicacuadrada.com>
2020-04-30 13:30:30 +0200

    finishing sign in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth,signInWithGoogle} from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 
@@ -14,12 +14,18 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
+        const {email,password} = this.state;
 
-        this.setState({email:'',password:''})
-
-    }
+        try {
+            await auth.signInWithEmailAndPassword(email,password);
+            // because we don't need the state anymore
+            this.setState({email:'',password:''});
+        } catch (error){
+            console.log(error);
+        }
+    };
 
     handleChange = event => {
         const{value, name} = event.target;
