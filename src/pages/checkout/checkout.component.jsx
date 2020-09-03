b6da350b9dd3f2a68c9eafe4b6b2468be8222f8d
lolmos <lolmos@3oclock>
2020-09-03 11:27:33 +0200

    updated stripe integration

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
@@ -33,6 +34,12 @@ const CheckoutPage = ({cartItems, total})=> (
         <div className='total'>
             <span>TOTAL:${total}</span>
         </div>
+        <div className='test-warning'>
+            *Please user the following test credit card for payments*
+            <br/>
+            4242 4242 4242 4242 - Exp:01/20 - CVV:123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
    
 )
