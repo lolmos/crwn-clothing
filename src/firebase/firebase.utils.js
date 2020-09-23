b741989f0de206f98ec6059b3060be41dc33f41a
lolmos <lolmos@3oclock>
2020-09-23 11:44:12 +0200

    created spinner files and implemented the spinner higher order component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -18,8 +18,12 @@ export const createUserProfileDocument = async (userAuth,additionalData) => {
   if (!userAuth) return;
   // set the user ref to 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const collectionRef= firestore.collection('users');
 
   const snapShot = await userRef.get();
+  const collectionSnapshot = await collectionRef.get();
+  console.log({collectionSnapshot});
+
   
   if(!snapShot.exists){
     const {displayName, email} = userAuth;
@@ -38,6 +42,39 @@ export const createUserProfileDocument = async (userAuth,additionalData) => {
   
   return userRef;
 
+};
+
+export const addCollectionAndDocuments =async(collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+  console.log(collectionRef);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj =>{
+    const newDocRef = collectionRef.doc();
+    console.log(newDocRef);
+    batch.set(newDocRef,obj)
+  });
+
+  return await batch.commit()
+
+};
+
+export const convertCollectionsSnapshotToMaps = (collections)=> {
+  const transformedCollection = collections.docs.map(doc =>{
+    const {title,items} = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  });
+
+  return transformedCollection.reduce((accumulator, collection)=> {
+    accumulator[collection.title.toLowerCase()]= collection;
+    return accumulator
+  },{});
 }
 
 firebase.initializeApp(config);
