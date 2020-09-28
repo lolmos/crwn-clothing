4661b209667c900ff92d7345a47407c88d84fa21
lolmos <lolmos@3oclock>
2020-09-28 10:02:54 +0200

    promise pattern lesson. switched from firebase api method to a promise.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -27,11 +27,14 @@ class ShopPage extends React.Component {
     const {updateCollections} = this.props;
     const collectionRef = firestore.collection('collections');
 
-    this.unsusbcribeFromSnapShot = collectionRef.onSnapshot(async snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMaps(snapshot);
-      updateCollections(collectionsMap);
-      this.setState({loading:false})
-    })
+     collectionRef.get().then(
+      async snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMaps(snapshot);
+        updateCollections(collectionsMap);
+        this.setState({loading:false})
+      }
+     )
+  
   };
 
   render() {
