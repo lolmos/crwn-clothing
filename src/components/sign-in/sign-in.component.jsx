bf7bb696a5d8b47ef33af648c1f554ab430cb2f5
lolmos <lolmos@3oclock>
2020-10-15 12:35:24 +0200

    added email and google sign in sagas

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,58 +1,59 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {auth,signInWithGoogle} from '../../firebase/firebase.utils';
+
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions'
 import './sign-in.styles.scss';
 
 
 class SignIn extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state ={
-            email:'',
-            password:''
+        this.state = {
+            email: '',
+            password: ''
         }
     }
 
     handleSubmit = async event => {
         event.preventDefault();
-        const {email,password} = this.state;
+        const { emailSignInStart } = this.props;
+        const { email, password } = this.state;
 
-        try {
-            await auth.signInWithEmailAndPassword(email,password);
-            // because we don't need the state anymore
-            this.setState({email:'',password:''});
-        } catch (error){
-            console.log(error);
-        }
+        emailSignInStart(email, password);
     };
 
     handleChange = event => {
-        const{value, name} = event.target;
-        this.setState({[name]:value})
+        const { value, name } = event.target;
+        this.setState({ [name]: value })
     }
 
-    render(){
-        return(
+    render() {
+        const { googleSignInStart } = this.props;
+        return (
             <div className='sign-in'>
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
-            
+
                 <form onSubmit={this.handleSubmit}>
                     <FormInput name='email' type='email' label='email' value={this.state.email} handleChange={this.handleChange} required />
-                    
+
                     <FormInput name='password' type='password' label='password' value={this.state.password} handleChange={this.handleChange} required />
                     <div className='buttons'>
                         <CustomButton type='submit'>Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-                        {' '}
-                        Sign in with Google{' '}
+                        <CustomButton
+                            type='button'
+                            onClick={googleSignInStart}
+                            isGoogleSignIn
+                        >
+                            Sign in with Google
                         </CustomButton>
                     </div>
 
-                
+
                 </form>
-                
+
             </div>
 
 
@@ -62,6 +63,9 @@ class SignIn extends React.Component {
     }
 };
 
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart()),
+    emailSignInStart: (email, password) => dispatch(emailSignInStart({ email, password }))
+})
 
-
-export default SignIn;
+export default connect(null, mapDispatchToProps)(SignIn);
---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,30 @@
+import {takeLatest, call, put} from 'redux-saga/effects';
+
+import { firestore, convertCollectionsSnapshotToMaps } from '../../firebase/firebase.utils';
+
+import {
+    fetchCollectionsSuccess,
+    fetchCollectionsFailure
+} from './shop.actions';
+
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollectionsAsync(){
+    yield console.log('I am fired ');
+
+    try {    
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionsSnapshotToMaps, snapshot);
+        yield put(fetchCollectionsSuccess(collectionsMap))
+    } catch(error){
+        yield put(fetchCollectionsFailure(error.message))
+    }    
+}
+
+export function* fetchCollectionsStart(){
+    yield takeLatest(
+        ShopActionTypes.FETCH_COLLECTIONS_START,
+        fetchCollectionsAsync
+    );
+}
